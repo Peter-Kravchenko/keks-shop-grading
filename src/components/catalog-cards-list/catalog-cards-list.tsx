5ceaed3bd68499f3b8da769a353aa6ae887d4b43
Петр <kravchenko.pb.13@gmail.com>
2023-11-19 12:11:00 +0300

    Исправит верстку каталога

--- src/components/catalog-cards-list/catalog-cards-list.tsx
+++ src/components/catalog-cards-list/catalog-cards-list.tsx
@@ -31,22 +31,24 @@ function CatalogCardsList({ products }: CatalogCardsListProps): JSX.Element {
     <section className="catalog">
       <div className="container">
         <h2 className="visually-hidden">Каталог</h2>
-        <div className="catalog__wrapper">
-          <ul className="catalog__list">
-            {productsOnPage.length === 0 && <ProductNotFound />}
-            {productsOnPage.length > 0 &&
-              productsOnPage.map((product) => (
+
+        {productsOnPage.length === 0 && <ProductNotFound />}
+        {productsOnPage.length > 0 && (
+          <div className="catalog__wrapper">
+            <ul className="catalog__list">
+              {productsOnPage.map((product) => (
                 <ProductCard
                   key={product.id}
                   product={product}
                   productBlock={ProductBlock.Catalog}
                 />
               ))}
-          </ul>
-          <div className="catalog__button-wrapper">
-            {isShowMore ? <ShowMoreButton /> : <ToTheBeginingButton />}
+            </ul>
+            <div className="catalog__button-wrapper">
+              {isShowMore ? <ShowMoreButton /> : <ToTheBeginingButton />}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </section>
   );
